2050446a1a9a8472071ad826dff3eb240e021658
caseinpoint <drues.other.email@gmail.com>
2022-04-07 08:34:59 -0700

    ready for lecture

--- classes/hackbright2.py
+++ classes/hackbright2.py
@@ -1,14 +1,55 @@
 class Hackbrighter:
-    """A human at hackbright."""
+    """A person at Hackbright."""
 
-    loves_python = True
+    hours_per_week = 40
 
-    def __init__(self, name, title):
+    def __init__(self, name, title, pronouns="unspecified"):
         self.name = name
         self.title = title
+        self.pronouns = pronouns
 
     def introduce(self):
-        print(f"Hi! My name is {self.name}. I'm a {self.title}")
+        print(f"Hi! My name is {self.name}. My pronouns are {self.pronouns}.")
 
-class Student(Hackbrighter):
-    pass
+        if self.title[0].lower() in "aeiou":
+            print(f"I'm an {self.title} at Hackbright.")
+        else:
+            print(f"I'm a {self.title} at Hackbright.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Student(Hackbrighter):
+#     pass
