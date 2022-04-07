90c1843d5a16906136610ca5202d751353179ba0
caseinpoint <drues.other.email@gmail.com>
2022-04-06 22:17:18 -0700

    add demo file for each lecture

---
+++ classes/hackbright1.py
@@ -0,0 +1,18 @@
+class HackbrightStudent:
+    """A student at Hackbright."""
+
+    school = "Hackbright Academy"
+
+    def __init__(self, student_name, pronouns="unspecified"):
+        self.name = student_name
+        self.pronouns = pronouns
+        self.grades = {}
+
+    def introduce(self):
+        print(f"Hi! My name is {self.name}. My pronouns are {self.pronouns}.")
+
+    def add_grade(self, assignment, grade):
+        self.grades[assignment] = grade
+
+    def __repr__(self):
+        return f"<HackbrightStudent name={self.name} pronouns={self.pronouns}>"
