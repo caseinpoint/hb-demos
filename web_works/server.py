4881ac0219fc24459c44dd5ccae0522bebdb7116
caseinpoint <drues.other.email@gmail.com>
2022-04-14 15:47:32 -0700

    add checkboxes

--- web_works/server.py
+++ web_works/server.py
@@ -19,18 +19,24 @@ def show_get_form(method):
 
 @app.route('/form/submit', methods=['GET', 'POST'])
 def handle_get_form():
-    """Show the form data and the request object."""
+    """Show the form data and the request object.
+    
+    This function demonstrates why you might want to know information about
+    the request, itself.  I.e. a route that handles two different request
+    methods."""
 
     if request.method == 'GET':
         form_data = {'name': request.args.get('name'),
                      'quest': request.args.get('quest'),
                      'color': request.args.get('color'),
+                     'grenade': request.args.getlist('grenade'),
                      'next': '/form/POST'}
 
     else:
         form_data = {'name': request.form.get('name'),
                      'quest': request.form.get('quest'),
                      'color': request.form.get('color'),
+                     'grenade': request.form.getlist('grenade'),
                      'next': '/response'}
 
     return render_template('form-output.html', request=request, **form_data)
