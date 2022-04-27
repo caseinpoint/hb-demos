ea98d5642cccd32c3de8733887252cb075d64581
caseinpoint <drues.other.email@gmail.com>
2022-04-27 11:57:01 -0700

    add docstrings

--- sql/sql-alchemy-2/chaining.py
+++ sql/sql-alchemy-2/chaining.py
@@ -1,3 +1,5 @@
+"""A demonstration of two ways to write methods that allow chaining."""
+
 class Number:
     """A number."""
 
@@ -8,20 +10,51 @@ class Number:
         return self.value
 
     def add(self, num=1):
+        """Increment value by num.
+        
+        If we were to try to chain these methods, we would get an error:
+        >>> n = Number(1)
+        >>> n.add(1).result()
+        AttributeError: 'NoneType' object has no attribute 'result'
+        """
+
         self.value += num
+
 
 class SameNumber(Number):
-    """Returns the same instance for chaining."""
+    """A chainable number."""
 
     def add(self, num=1):
+        """Increment value by num.
+        
+        By returning self, we can chain method calls:
+        >>> sn = SameNumber(1)
+        >>> sn.add(1).result()
+        2
+        """
+
         self.value += num
+
         return self
 
+
 class NewNumber(Number):
-    """Returns a new instance for chaining.
-    
-    (This is similar to how flask_sqlalchemy.BaseQuery works)"""
+    """A chainable number."""
 
     def add(self, num=1):
+        """Increment value by num.
+
+        By returning a new instance, we can chain method calls without mutating
+        the original instance:
+        >>> nn = NewNumber(1)
+        >>> nn.add(1).result()
+        2
+        >>> nn.result()
+        1
+
+        (This is similar to how flask_sqlalchemy.BaseQuery works)
+        """
+
         new_num = self.value + num
+
         return NewNumber(value=new_num)
