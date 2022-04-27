4cf4e65f80ababe7d339527f3e210485599ba8fd
caseinpoint <drues.other.email@gmail.com>
2022-04-27 11:37:07 -0700

    minor modifications during lecture

--- sql/sql-alchemy-1/model.py
+++ sql/sql-alchemy-1/model.py
@@ -9,13 +9,13 @@ db = SQLAlchemy()
 
 
 class Cat(db.Model):
-    """Feline catus."""
+    """Felis catus."""
 
     __tablename__ = "cats"
 
     cat_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
-    color = db.Column(db.String(10), nullable=True)
+    color = db.Column(db.String(50), nullable=True)
     hunger = db.Column(db.Integer, nullable=False, default=20)
 
     def __repr__(self):
@@ -26,7 +26,7 @@ class Cat(db.Model):
     def greet(self):
         """Greet using name."""
 
-        return f"I am {self.name}"
+        return f"Meow! I'm {self.name}"
 
     def feed(self, units=10):
         """Nom nom nom."""
@@ -35,8 +35,16 @@ class Cat(db.Model):
         self.hunger = max(self.hunger, 0)
 
 
+# class CatOwner(db.Model):
+#     __tablename__ = "owners"
+
+#     owner_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     name = db.Column(db.String(50), nullable=False)
+
 def connect_to_db(app, db_name):
     """Connect to database."""
+
+    print('Connecting to db...', end=' ')
 
     app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///{db_name}"
     app.config["SQLALCHEMY_ECHO"] = True
@@ -44,6 +52,8 @@ def connect_to_db(app, db_name):
 
     db.app = app
     db.init_app(app)
+
+    print('Connected!')
 
 
 if __name__ == "__main__":
