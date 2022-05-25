6c313d2de395f3351a98293744dd26928735f4df
caseinpoint <drues.other.email@gmail.com>
2022-05-25 15:22:26 -0700

    initial commit

---
+++ graphs/friends.py
@@ -0,0 +1,161 @@
+"""Example of an undirected graph."""
+
+from collections import deque
+
+
+class PersonNode:
+    """Node in a graph representing a person."""
+
+    def __init__(self, name, adjacent=None):
+        """Create a person node with friends adjacent"""
+
+        if adjacent is None:
+            adjacent = set()
+
+        assert isinstance(adjacent, set), "adjacent must be a set!"
+
+        self.name = name
+        self.adjacent = adjacent
+
+    def __repr__(self):
+        """Debugging-friendly representation"""
+
+        return f"<PersonNode: {self.name}>"
+
+
+class FriendGraph:
+    """Graph holding people and their friendships."""
+
+    def __init__(self):
+        """Create an empty graph"""
+
+        self.nodes = set()
+
+    def __repr__(self):
+        return f"<FriendGraph: { {n.name for n in self.nodes} }>"
+
+    def add_person(self, person):
+        """Add a person to our graph"""
+
+        self.nodes.add(person)
+
+    def set_friends(self, person1, person2):
+        """Set two people as friends"""
+
+        person1.adjacent.add(person2)
+        person2.adjacent.add(person1)
+
+    def add_people(self, people_list):
+        """Add a list of people to our graph"""
+
+        for person in people_list:
+            self.add_person(person)
+
+    def remove_person(self, person):
+        """Remove a person from the graph."""
+
+        # remove all edges that point to person
+        for friend in person.adjacent:
+            friend.adjacent.remove(person)
+
+        # remove person from the graph
+        self.nodes.remove(person)
+
+    def are_connected(self, person1, person2):
+        """Are two people connected? Breadth-first search."""
+
+        possible_nodes = deque()
+        seen = set()
+        possible_nodes.append(person1)
+        seen.add(person1)
+
+        while len(possible_nodes) > 0:
+            person = possible_nodes.popleft()
+            print("checking", person)
+            if person is person2:
+                return True
+            else:
+                # for friend in person.adjacent - seen:
+
+                for friend in person.adjacent:
+                    if friend not in seen:
+                        possible_nodes.append(friend)
+                        seen.add(friend)
+                        print("added to queue:", friend)
+        return False
+
+    def are_connected_recursive(self, person1, person2, seen=None):
+        """Are two people friends? Recursive depth-first search."""
+
+        if seen is None:
+            seen = set()
+
+        if person1 is person2:
+            return True
+
+        seen.add(person1)  # Keep track that we've visited here
+        print("adding", person1)
+        # check that seen is the same as other recursive calls
+        print("id of seen:", id(seen))
+        print("seen:", seen, '\n')
+
+        for person in person1.adjacent:
+
+            if person not in seen:
+
+                if self.are_connected_recursive(person, person2, seen):
+                    return True
+
+        return False
+
+    def verbose_are_connected_recursive(self, person1, person2, seen=None):
+        """Are two people friends? Recursive depth-first search."""
+
+        if not seen:
+            seen = set()
+
+        if person1 is person2:
+            print(f"\nreturning True - {person1.name} is {person2.name}")
+            return True
+
+        seen.add(person1)  # Keep track that we've visited here
+        print("adding", person1)
+
+        for person in person1.adjacent:
+
+            if person not in seen:
+
+                print(
+                    f"calling method on {person1.name}'s friend {person.name} with {person2.name}"
+                )
+                if self.verbose_are_connected_recursive(person, person2, seen):
+                    print(f"\nreturning True from checking {person.name}")
+                    return True
+
+        print(f"returning False from checking {person1.name}")
+        return False
+
+
+# Add sample friends
+ankha = PersonNode("Ankha")
+canberra = PersonNode("Canberra")
+beau = PersonNode("Beau")
+drake = PersonNode("Drake")
+genji = PersonNode("Genji")
+erik = PersonNode("Erik")
+hamphrey = PersonNode("Hamphrey")
+ike = PersonNode("Ike")
+jay = PersonNode("Jay")
+
+friends = FriendGraph()
+friends.add_people([ankha, canberra, beau, drake, erik, hamphrey, ike, jay])
+
+friends.set_friends(ankha, canberra)
+friends.set_friends(ankha, beau)
+friends.set_friends(ankha, drake)
+friends.set_friends(canberra, beau)
+friends.set_friends(drake, canberra)
+friends.set_friends(drake, genji)
+friends.set_friends(beau, erik)
+friends.set_friends(hamphrey, ike)
+friends.set_friends(hamphrey, jay)
