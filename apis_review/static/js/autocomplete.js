6fb3089bf67e1dc413acb73310695222625171dc
caseinpoint <drues.other.email@gmail.com>
2022-05-25 15:20:49 -0700

    added logic to prevent search upon selecting autocomplete suggestion

--- apis_review/static/js/autocomplete.js
+++ apis_review/static/js/autocomplete.js
@@ -1,18 +1,23 @@
 'use strict';
 
+let prevQuery = '';
+
 const handleInput = (evt) => {
     const query = evt.target.value;
-    if (query.length > 0) {
-        const queryString = new URLSearchParams({ query }).toString();
+    const diffLength = prevQuery.length - query.length;
 
+    if (query.length !== 0 && (diffLength === 1 || diffLength === -1)) {
+        
+        const queryString = new URLSearchParams({ query }).toString();
+        
         fetch(`/autocomplete.json?${queryString}`)
         .then((response) => response.json())
         .then((resultsArray) => {
-            console.log(resultsArray);
-
+            // console.log(resultsArray);
+            
             const dataList = document.querySelector('#autocomplete');
             dataList.innerHTML = '';
-
+            
             for (const description of resultsArray) {
                 const opt = document.createElement('option');
                 opt.value = description;
@@ -21,6 +26,8 @@ const handleInput = (evt) => {
             }
         });
     }
+
+    prevQuery = query;
 };
 
 document.querySelector('#query').addEventListener('input', handleInput);
