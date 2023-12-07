c686c9bbb7fcd3fd0078539d79f41e605a7024b2
caseinpoint <drues.other.email@gmail.com>
2023-12-07 13:08:45 -0700

    comment out for next lecture

--- classes/classes1.py
+++ classes/classes1.py
@@ -1,50 +1,62 @@
-# crenshaw = {
-#     "price": 2.00,
-#     "color": "green",
-#     "source": ["USA", "Brazil"],
-#     "seedless": False,
-# }
+crenshaw = {
+    "price": 2.00,
+    "color": "green",
+    "source": ["USA", "Brazil"],
+    "seedless": False,
+}
+
+def buy_melon(melon_type):
+    """Buy a melon, checking source country and price."""
+
+    print(f'You bought a melon from {melon_type["source"]} for ${melon_type["price"]}.')
+
+buy_melon(crenshaw)
+
 # watermelon = {
 #     "price": 3.00,
 #     "size": "large",
 #     "comes_from": ["USA", "Spain"],
 # }
-
-# def buy_melon(melon_type):
-#     """Buy a melon, checking source country and price."""
-#     print(f'You bought a melon from {melon_type["source"]} for ${melon_type["price"]}.')
-
-print(__name__)
+# buy_melon(watermelon)
 
 
-class Cat:
-    """A cat. Felis catus"""
-
-    species = 'cat'
-
-    def __init__(self, name, color):
-        self.name = name.title()
-        self.color = color
-
-    def __repr__(self):
-        return f'<Cat name={self.name}>'
-
-    def speak(self):
-        print(f'Meow! I am {self.name} the {self.species}.')
-
-    def rename(self, new_name):
-        self.name = new_name
-
-    def dance(self, times=3):
-        return ':prance:' * times
-
-    def graduate(self):
-        self.name = 'Dr. ' + self.name
-        self.speak()
-        # self.name = f'Dr. {self.name}'
 
 
-if __name__ == '__main__':
-    felix = Cat('felix', 'tuxedo')
 
-    kitty = Cat('KITTY', 'tabby')
+# class Cat:
+#     """A cat."""
+
+#     species = 'cat'
+#     diet = 'carnivore'
+#     fav_foods = ['kibble', 'canned tuna']
+
+#     def __init__(self, name):
+#         self.name = name
+
+#     def __repr__(self):
+#         return f'<Cat object name={self.name}>'
+
+#     def speak(self):
+#         print(f'Meow! I am {self.name} the {self.species}')
+
+#     def id_check(self, check):
+#         print('id(self)=', id(self), 'check=', check)
+
+#     def dance(self):
+#         return ':prance, prance, prance:'
+
+#     def graduate(self):
+#         self.name = f'Dr. {self.name}'
+#         self.speak()
+
+# felix = Cat()
+# felix.name = 'Felix'
+# check = id(felix)
+# felix.id_check(check)
+# kitty = Cat()
+# kitty.name = 'Kitty'
+
+
+# def make_cat():
+#     cat_instance = Cat()
+#     return cat_instance
